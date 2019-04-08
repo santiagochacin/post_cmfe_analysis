51723a22773c96f954eb37181d203a2efddc4bd3
chief <chief>
2019-04-08 08:32:44 -0500

    changes

--- jjm_post_cnmfe_fns.py
+++ jjm_post_cnmfe_fns.py
@@ -9,19 +9,19 @@ from tqdm import tqdm
 def get_neuron_pairs(num_neurons):
 	#create list of tuples for neuron pairs
 	neuron_pairs = []
-	for i in range(len(coordinates)):
-		for j in range(len(coordinates)):
+	for i in range(num_neurons):
+		for j in range(num_neurons):
 			if i != j: 
-			neuron_pairs.append((i,j))
+				neuron_pairs.append((i,j))
 	return()
 
 
 def get_r_coeffs_distances_paired(neuron_pairs, C, coordinates):
 	pairwise = {}
 	for pair in tqdm(neuron_pairs):
-    #calculate pearsons r
-    	r = stats.pearsonr(mat_results['C'][pair[0]], mat_results['C'][pair[1]])
-    #calc euclidian distance 
-    	distance = dist.euclidean(coordinates[pair[0]]['CoM'], coordinates[pair[1]]['CoM'])
-    	pairwise[pair] = (r, distance)
+	#calculate pearsons r
+		r = stats.pearsonr(mat_results['C'][pair[0]], mat_results['C'][pair[1]])
+	#calc euclidian distance 
+		distance = dist.euclidean(coordinates[pair[0]]['CoM'], coordinates[pair[1]]['CoM'])
+		pairwise[pair] = (r, distance)
 	return(pairwise)
