b912cb9cc5fc13c984b06ba6f8d09bcb380b2230
marburyvthesea <jjm2128@gmail.com>
2019-05-01 18:18:06 -0500

    changes

--- align_msCam_tobehavior.py
+++ align_msCam_tobehavior.py
@@ -7,12 +7,73 @@ def import_test():
 	print ('imported!')
 	return()
 
-def align_and_return_ezTrack(tracking_df, timestamps_df_path):
+def return_triggered_events(trace_to_align, trigger_points, sample_window):
+	"""
+	Inputs:
+	sample_window is tuple (samples before, samples after)
+	"""
+	traces_out = {}
+	for point in trigger_points:
+		traces_out[point] = trace_to_align[point-sample_window[0]:point+sample_window[1]]
+	traces_out_df = pd.DataFrame(traces_out)
+
+	return(traces_out_df)
+
+
+def count_events_in_array_threshold(selection, Fs, time_to_wait, threshold=0, up=True):
+	""" Counts the number of events (e.g action potentials (AP)) in the current trace.
+	Arguments:
+	trace -- 1d numpy array
+	dt -- sampling interval
+	threshold -- (optional) detection threshold (default = 0).
+	up -- (optional) True (default) will look for upward events, False downwards.
+
+	Returns:
+	An integer with the number of events.
+	Examples:
+	count_events(500,1000) returns the number of events found between t=500 ms and t=1500 ms
+	above 0 in the current trace and shows a stf marker.
+	count_events(500,1000,0,False,-10,i) returns the number of events found below -10 in the 
+	trace 
+	"""
+	# add in a minimum bout length?
+	time_threshold_samples = int(time_to_wait*Fs)
+	# algorithm to detect events
+	EventCounter,i = 0,0 # set counter and index to zero
+	# list of sample points
+	sample_points = []
+	# choose comparator according to direction:
+	if up:
+		comp = lambda a, b: a > b
+	else:
+		comp = lambda a, b: a < b
+	# loop over array
+	while i<len(selection):
+
+		if comp(float(selection[i]),float(threshold)):
+
+			to_add = i
+			bout_start = i
+
+			while i<len(selection) and comp(selection[i],threshold):
+				i+=1 # skip values if index in bounds AND until the value is below/above threshold again
+
+			bout_end = i 
+			if time_threshold_samples < (bout_end - bout_start):
+				EventCounter +=1
+				sample_points.append(bout_start)   
+
+		else:
+			i+=1
+
+	return (EventCounter, sample_points)
+
+def align_and_return_ezTrack(tracking_df, timestamps_df_path, length_CNMFE_data):
 
 	timestamps_df = pd.read_table(timestamps_df_path)
 
 	# timestamps of msCam and behavCam
-	msCam_timestamps = timestamps_df[timestamps_df['camNum'] == 0].set_index('frameNum')
+	msCam_timestamps = timestamps_df[timestamps_df['camNum'] == 0].set_index('frameNum')[0:length_CNMFE_data]
 	behavCam_timestamps = timestamps_df[timestamps_df['camNum'] == 1].set_index('frameNum')
 
 	# length to align is length of tracking df
@@ -24,14 +85,16 @@ def align_and_return_ezTrack(tracking_df, timestamps_df_path):
 	# add following parameters to msCam data frame
 	sys_clocks_behavCam = [0]
 	behavCam_frames = [1]
+	msCam_frames = [1]
 	dist_btw_frames = [0]
 	velocity = [0]
 
 	# need to check alignemnt of new series here
-	for msCam_frame in tqdm(range(2, len(msCam_timestamps)+1)):
+	for msCam_frame in tqdm(range(2, length_CNMFE_data+1)):
 		#get sys clock time of each miniscope recorded frame
 		#find closest behav cam frame by sys clock time, 
 		sys_clock_msCam = msCam_timestamps['sysClock'].loc[msCam_frame]
+		msCam_frames.append(list(msCam_timestamps.loc[msCam_timestamps['sysClock'] == sys_clock_msCam].index)[0])
 		behavCam_frame = list(behav_trimmed.iloc[(behav_trimmed['sysClock']-sys_clock_msCam).abs().argsort()[:1]].index)[0]
 		behavCam_frames.append(behavCam_frame)
 		sys_clocks_behavCam.append(behav_trimmed.loc[behavCam_frame]['sysClock'])
@@ -40,23 +103,20 @@ def align_and_return_ezTrack(tracking_df, timestamps_df_path):
 		#velocity is the difference in the distance between frames divived by the sysClcok difference
 		delta_t = behav_trimmed.loc[behavCam_frame]['sysClock']-behav_trimmed.loc[behavCam_frame-1]['sysClock']
 		velocity.append(delta_d/(delta_t/1000))
-
+	# reset initial clock value to 0 
+	msCam_timestamps['sysClock'][1] = 0
 	# add final values to df
 	msCam_timestamps['velocity'] = velocity
 	msCam_timestamps['Distance'] = dist_btw_frames
 	msCam_timestamps['behavCam_frames'] = behavCam_frames
+	msCam_timestamps['msCam_frames'] = msCam_frames
 	msCam_timestamps['sys_clocks_behavCam'] = sys_clocks_behavCam
 
 	return(msCam_timestamps)
 
 
-
-
-
-#input timestamp data from miniscope DAQ and behaviorsoft tracking data
-
-
 def align_and_return(tracking_df_path, timestamps_df_path):
+#input timestamp data from miniscope DAQ and behaviorsoft tracking data
 
 	tracking_df = pd.read_table(tracking_df_path, header=None, names=['dist_btw_frames'])
 
