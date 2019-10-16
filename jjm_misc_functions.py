c65c5dcb8ded1409a30b41325fa8c94e2e2a5f43
marburyvthesea <jjm2128@gmail.com>
2019-10-16 01:58:55 -0500

    changes

--- jjm_misc_functions.py
+++ jjm_misc_functions.py
@@ -5,6 +5,12 @@ import numpy as np
 
 
 
+
+
+
+
+
+
 def F_F0(fluorescence_power, Fs, sliding_baselne_length):
 	"""calculate F/F0 as a sliding baseline
 		Inputs: sliding_baseline_length (in seconds)
@@ -13,7 +19,8 @@ def F_F0(fluorescence_power, Fs, sliding_baselne_length):
 	F_F0_array = []
 	for point in range(int(baseline_length_samples), len(fluorescence_power)):
 		F0 = np.mean(fluorescence_power[int(point-baseline_length_samples):int(point)])
-		F_F0 = fluorescence_power[point]/F0
+		F_F0 = (fluorescence_power[point]-F0)/F0
+		print(F_F0)
 		F_F0_array.append(F_F0)
 
 	return(np.array(F_F0_array))
